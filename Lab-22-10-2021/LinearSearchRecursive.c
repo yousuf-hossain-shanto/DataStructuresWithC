042c864ca25b889697090b7ebd907fd76829e96e
Yousuf Hossain <yhshanto@Yousufs-MacBook-Pro.local>
2021-10-22 20:39:59 +0600

    Added Linear search recursive

--- Lab-22-10-2021/LinearSearchRecursive.c
+++ Lab-22-10-2021/LinearSearchRecursive.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-int binarySearch(int pArr[], int searchKey, int startEn, int endEn)
+int linearSearch(int mainArr[], int count, int search, int currentIndex)
 {
-    if (startEn <= endEn)
+    if (currentIndex < count)
     {
-        int mid = ((startEn + endEn) / 2);
-        if (searchKey == pArr[mid])
-            return mid;
-
-        if (searchKey < pArr[mid])
-        {
-            return binarySearch(pArr, searchKey, startEn, mid - 1);
-        }
-        return binarySearch(pArr, searchKey, mid + 1, endEn);
+        if (mainArr[currentIndex] == search)
+            return currentIndex;
+        return linearSearch(mainArr, count, search, currentIndex + 1);
     }
     return -1;
 }
@@ -31,8 +25,10 @@ int main()
 
     printf("Enter search key => ");
     scanf("%d", &x);
-    // Binary Search
-    int result = binarySearch(searchableArr, x, 0, n - 1);
+
+    // Linear Search
+    int result = linearSearch(searchableArr, n, x, 0);
+
     if (result != -1)
     {
         printf("%d found at %d position", x, result);
